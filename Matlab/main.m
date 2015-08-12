b4879e1199bac26b10fae8dc4b403372a1f0f389
Andrew Gaus <andrewgaus815@gmail.com>
2015-08-11 21:22:22 -0400

    reworked main.m. Added api_key.mat which will only be available local side in order to avoid sharing my api_key.

--- Matlab/main.m
+++ Matlab/main.m
@@ -1,2 +1,16 @@
 % Riot Games API Challenge: AP Items Analysis
 % Author: Andrew Gaus (tbnboy815)
+
+clear all
+close all
+
+API_KEY = load('api_key.mat');
+
+url = 'https://na.api.pvp.net';
+api = '/api/lol/na/v1.4/summoner/by-name/';
+input = 'tbnboy815';
+key = ['?api_key=' API_KEY];
+
+testurl = [url,api,input,key]
+
+stru = webread(testurl)
